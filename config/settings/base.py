d8837928778ff1e051216ca434c75338f6c365b4
Junhyun <puppy0513@naver.com>
2021-12-07 00:38:06 +0900

    hyperparameter tuning_1

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -114,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
